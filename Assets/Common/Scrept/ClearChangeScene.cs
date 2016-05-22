49657a8abdd9393cb7937a5241e5c0a031ac930c
佐藤太一 <okms310@gmail.com>
2016-05-22 19:27:49 +0900

    タイトルシーンお引越し完了

---
+++ Assets/Common/Scrept/ClearChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ClearChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.JoystickButton17)) {
+			SceneManager.LoadScene ("Title_scene");
+		}
+	
+	}
+}
